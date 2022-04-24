fb84a386a7d3bb3c71d7ebb51f5d696a4d233182
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-04-24 15:10:12 +0200

    function return type

---
+++ section-2/function-return-type.ts
@@ -0,0 +1,16 @@
+/**
+ * Add two numbers together.
+ * Function return type can be explicitly set, 
+ * even though TS is able to infere the return type, it is a best-practices to 
+ * explicitly set it.
+ * @param n1 number
+ * @param n2 number
+ * @returns number
+ */
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+console.log('Result:', add(1, 2));
+
+export { };
