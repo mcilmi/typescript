80b3053f0d2e0171d8828fbbff33f4297c9e7bf0
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-04-25 19:33:32 +0200

    debugging in vscode. End of section 3.

--- section-3/dist/app.js
+++ section-3/dist/app.js
@@ -1,1 +1,7 @@
 console.log('section 3');
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    // let userName = "Madinah";
+    console.log('button clicked!');
+});
+//# sourceMappingURL=app.js.map
--- section-3/src/app.ts
+++ section-3/src/app.ts
@@ -1,1 +1,8 @@
 console.log('section 3');
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+    // let userName = "Madinah";
+    console.log('button clicked!');
+});
