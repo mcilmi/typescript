5d7dac0076f500028e54769d4661d89538f9aa28
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-05-03 20:39:10 +0200

    optional properties and parameters

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -4,6 +4,7 @@
 
 interface Named {
     readonly name: string;
+    outputName?: string;// Optional property
 }
 
 // It is possible to extend existing interfaces, thus composition is achieved. 
@@ -25,20 +26,25 @@ console.log('add(10,22):', add(10, 22));
 
 class Person implements Greetable {
     name: string;
-    age: number;
+    age: number = 7;
 
-    constructor(name: string, age: number) {
+    constructor(name?: string) {
         this.name = name;
     }
 
     greet(phrase: string): void {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+
+            console.log(phrase + ' ' + this.name);
+        } else {
+            console.log('Assalamu caleykum, bilaa magac ayaan ahay :(')
+        }
     }
 
 }
 
 let user1: Greetable;
 
-user1 = new Person('Madinah Maxamed', 7);
+user1 = new Person('Madinah Maxamed');
 // user1.name = 'new name'; // Fails due to readonly
 user1.greet('Assalamu caleykum, magaceygu waa');
