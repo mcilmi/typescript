7c3a758f1698bcedc727f8259729bb953e8fa504
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-05-03 20:33:08 +0200

    interfaces function type

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -6,11 +6,22 @@ interface Named {
     readonly name: string;
 }
 
-// It is possible to extend existing interface, thus composition is achieved. 
+// It is possible to extend existing interfaces, thus composition is achieved. 
 interface Greetable extends Named {
     age: number;
     greet(phrase: string): void;
 }
+
+interface AddFn {
+    (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => n1 + n2;
+
+console.log('add(10,22):', add(10, 22));
+
 
 class Person implements Greetable {
     name: string;
