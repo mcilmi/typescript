4c34a2363bf6121f4f93bec4fcef6101793835a2
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-05-03 16:42:38 +0200

    class, constructor, instance methods and fields.

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -1,1 +1,17 @@
-console.log('init section 5');
+
+class Department {
+    private name: string;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+}
+
+// Init object from class blueprint
+
+const accounting = new Department('Accounting');
+accounting.describe();
