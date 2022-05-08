43d91719dc1c9ce8d003e76643f1baacb2b7bc6d
Maxamed Cilmi <mohamedshabeel@gmail.com>
2022-05-08 10:51:55 +0200

    init. generic-types.

---
+++ section-7/src/generic-types.ts
@@ -0,0 +1,20 @@
+/**
+ * Built-it Generic types are
+ * Syntax: MainType<subType>
+ * E.g: Array<string> = string[]
+ */
+// const names = ['Mikal','Madinah'];
+const names: Array<string> = [];
+// Due to the defined type, the built-in methods for the type is available
+// names[0].split('-').join(' ');
+
+// Promise are by default generics, but types can be defined, such that the expected result is know.
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Task is done!');
+    }, 4000);
+});
+
+promise.then(result => console.log(result.split(' ').join('')));
+
+export { };
